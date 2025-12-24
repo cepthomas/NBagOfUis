27508a948f30a4a61a03e7490f2794864f0822d2
cepthomas <cepthomas@gmail.com>
2025-12-24 10:32:52 -0600

    Add type editor.

--- TypeEditors.cs
+++ TypeEditors.cs
@@ -10,9 +10,12 @@ using System.Windows.Forms;
 using System.Windows.Forms.Design;
 using Ephemera.NBagOfTricks;
 
+// Shut up warnings in this file. Using manual checking where needed.
+#pragma warning disable CS8602
 
 namespace Ephemera.NBagOfUis
 {
+    //----------------------------------------------------------------
     /// <summary>Generic property editor for lists of strings.</summary>
     public class StringListEditor : UITypeEditor
     {
@@ -43,6 +46,7 @@ namespace Ephemera.NBagOfUis
         public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext? context) { return UITypeEditorEditStyle.DropDown; }
     }
 
+    //----------------------------------------------------------------
     /// <summary>Selector for monospaced fonts.</summary>
     public class MonospaceFontEditor : UITypeEditor
     {
@@ -60,6 +64,85 @@ namespace Ephemera.NBagOfUis
         public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext? context) { return UITypeEditorEditStyle.Modal; }
     }
 
+    //----------------------------------------------------------------
+    /// <summary>Select from list supplied to cache. Value can be int or string.</summary>
+    public class GenericListTypeEditor : UITypeEditor
+    {
+        #region Store global property options here.
+        static readonly Dictionary<string, List<string>> _options = [];
+
+        /// <summary>These get set by the client.</summary>
+        public static void SetOptions(string propName, List<string> options)
+        {
+            _options[propName] = options;
+        }
+
+        public static List<string> GetOptions(string propName)
+        {
+            if (!_options.TryGetValue(propName, out var result))
+            {
+                throw new InvalidOperationException($"No options provided for property {propName}");
+            }
+            return result;
+        }
+        #endregion
+
+        /// <summary>Standard property editor.</summary>
+        public override object? EditValue(ITypeDescriptorContext? context, IServiceProvider provider, object? value)
+        {
+            IWindowsFormsEditorService? _service = provider.GetService(typeof(IWindowsFormsEditorService)) as IWindowsFormsEditorService;
+
+            var propName = context.PropertyDescriptor.Name;
+            var propType = context.PropertyDescriptor.PropertyType;
+            var options = GetOptions(propName);
+
+            var lb = new ListBox
+            {
+                Width = 150,
+                SelectionMode = SelectionMode.One
+            };
+            lb.Click += (_, __) => _service!.CloseDropDown();
+            options.ForEach(v => lb.Items.Add(v));
+            _service!.DropDownControl(lb);
+
+            var ret = propType switch
+            {
+                var t when t == typeof(int) || t == typeof(int?) => lb.SelectedIndex,
+                var t when t == typeof(string) => lb.SelectedItem,
+                _ => throw new InvalidOperationException($"Property {propName} type must in or string")
+            };
+
+            return ret;
+        }
+
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext? context) { return UITypeEditorEditStyle.DropDown; }
+    }
+
+    //----------------------------------------------------------------
+    /// <summary>Convert between list int and string versions.</summary>
+    public class GenericConverter : Int64Converter
+    {
+        /// <summary>int to string</summary>
+        public override object? ConvertTo(ITypeDescriptorContext? context, System.Globalization.CultureInfo? culture, object? value, Type destinationType)
+        {
+            if (context is null || context.Instance is null || value is not int || (int)value < 0) { return base.ConvertTo(context, culture, value, destinationType); }
+            var propName = context.PropertyDescriptor.Name;
+            var options = GenericListTypeEditor.GetOptions(propName);
+            return options[(int)value];
+        }
+
+        /// <summary>string to int</summary>
+        public override object? ConvertFrom(ITypeDescriptorContext? context, System.Globalization.CultureInfo? culture, object value)
+        {
+            if (context is null || context.Instance is null || value is not int || (int)value < 0) { return base.ConvertFrom(context, culture, value); }
+            var propName = context.PropertyDescriptor.Name;
+            var options = GenericListTypeEditor.GetOptions(propName);
+            var res = options.FirstOrDefault(ch => ch == (string)value);
+            return res;
+        }
+    }
+
+    //----------------------------------------------------------------
     /// <summary>Broken - don't use.</summary>
     public class PathListEditor : UITypeEditor // TODO doesn't work yet.
     {
